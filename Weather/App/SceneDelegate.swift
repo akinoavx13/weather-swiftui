a0f9bb20bacf4435e900b6a2fcdcfda67b99c5e5
Maxime Maheo <imaxame@gmail.com>
2020-04-17 21:33:02 +0200

    Fix crash : “Default app has already been configured.”

--- Weather/App/SceneDelegate.swift
+++ Weather/App/SceneDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SwiftUI
-import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -20,8 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        FirebaseApp.configure()
         
         AppDependency.shared.registerServices()
         
